d3da90c1cdf7e4b42098ce62071a028cacbc767b
VillainNumberOne <66220826+VillainNumberOne@users.noreply.github.com>
2022-10-18 21:29:55 +0300

    new tests

--- person_service/tests/test_api.py
+++ person_service/tests/test_api.py
@@ -1,4 +1,5 @@
 import os
+import json
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "person_service.settings")
 
 import django
@@ -13,12 +14,71 @@ def test_get_all():
     request = factory.get('/api/v1/persons')
     response = person_service_api(request)
     assert response.status_code == 200
-    # assert 2 == 2
 
-def test_get_id():
+def test_get_id_existing():
     factory = RequestFactory()
-    request = factory.get('/api/v1/persons/100000')
+    data = {
+        "name": "test",
+        "age": 0,
+        "address": "test",
+        "work": "test"
+    }
+    request = factory.post('/api/v1/persons/', data, content_type='application/json')
     response = person_service_api(request)
-    print(response.content)
+
+    location = response.headers["Location"]
+    id = int(location[location.rfind("/") + 1:])
+
+    request = factory.get('/api/v1/persons/')
+    response = person_service_api(request, id)
+
     assert response.status_code == 200
-    # assert 2 == 2
+
+    request = factory.delete('/api/v1/persons/')
+    person_service_api(request, id)
+
+def test_get_id_non_existing():
+    factory = RequestFactory()
+    request = factory.get('/api/v1/persons/')
+    response = person_service_api(request, -1)
+    assert response.status_code == 404
+
+def test_put():
+    factory = RequestFactory()
+    data = {
+        "name": "test",
+        "age": 0,
+        "address": "test",
+        "work": "test"
+    }
+    request = factory.post('/api/v1/persons/', data, content_type='application/json')
+    response = person_service_api(request)
+
+    assert response.status_code == 201
+
+    location = response.headers["Location"]
+    id = int(location[location.rfind("/") + 1:])
+    request = factory.delete('/api/v1/persons/')
+    person_service_api(request, id)
+
+def test_put_bad():
+    factory = RequestFactory()
+    data = {
+        "nice": "ok",
+    }
+    request = factory.post('/api/v1/persons/', data, content_type='application/json')
+    response = person_service_api(request)
+
+    assert response.status_code == 400
+
+def test_delete_existing():
+    factory = RequestFactory()
+    request = factory.delete('/api/v1/persons/')
+    response = person_service_api(request, 1)
+    assert response.status_code == 204
+
+def test_delete_non_existing():
+    factory = RequestFactory()
+    request = factory.delete('/api/v1/persons/')
+    response = person_service_api(request, -1)
+    assert response.status_code == 204
